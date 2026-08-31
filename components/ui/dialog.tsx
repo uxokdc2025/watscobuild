b80413bb94df6b6dd341afdc49a22c06969d26c5
“David <designer@uxokdc.com>
2026-08-31 17:06:28 -0500

    Apply shared motion to dialog drawers

--- components/ui/dialog.tsx
+++ components/ui/dialog.tsx
@@ -64,9 +64,7 @@ function DialogContent({
         data-slot="dialog-content"
         className={cn(
           "fixed top-[50%] left-[50%] z-50 grid w-full max-w-[calc(100%-2rem)] translate-x-[-50%] translate-y-[-50%] gap-4 rounded-lg border bg-background p-6 shadow-lg duration-200 outline-none data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95 data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95 sm:max-w-lg",
-          drawerSide && (drawerSide === "left"
-            ? "data-[state=open]:drawer-panel-left-enter data-[state=closed]:drawer-panel-left-exit data-[state=open]:animate-none data-[state=closed]:animate-none"
-            : "data-[state=open]:drawer-panel-right-enter data-[state=closed]:drawer-panel-right-exit data-[state=open]:animate-none data-[state=closed]:animate-none"),
+          drawerSide && (drawerSide === "left" ? "drawer-dialog-left" : "drawer-dialog-right"),
           className
         )}
         {...props}
